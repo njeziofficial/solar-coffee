a700afea5cdcf7609f5b0ca74470086480c2dc73
njezioffifcal <john@cygnusspin.com>
2020-12-27 21:13:09 +0100

    Set up Postgres connection

---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Data
+{
+   public class SolarDbContext : IdentityDbContext
+    {
+        public SolarDbContext()
+        {}
+
+        public SolarDbContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
+    }
+}
